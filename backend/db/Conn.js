04aef0de2d50e6b841e83697559585b9a153f66d
MarceloUrsulino <marceloursulino40@gmail.com>
2026-09-06 22:04:16 -0300

    feat: configuração do banco, db.

---
+++ backend/db/Conn.js
@@ -0,0 +1,9 @@
+const Squelize = require('sequelize')
+
+const sequelize = new Squelize.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    timezone: "-03:00"
+})
+
+module.exports = sequelize
