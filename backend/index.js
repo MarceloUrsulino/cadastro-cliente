379b9424f6b76b9334a0cd32bdaafa2ac4f9915b
MarceloUrsulino <marceloursulino40@gmail.com>
2026-09-08 22:11:08 -0300

    feat: index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,24 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 
+const db = require('./db/Conn')
+
 app.use(express.json())  
+
+
+
+
+
+
+db.sync().then(() => {
+
+    console.log('Conectou')
+    app.listen(3000)
+
+}).catch(err =>{
+
+    console.log(err)
+})
 
 
