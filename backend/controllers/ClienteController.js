2e2a39448985c8dc7635d9173be58c29aa583a76
MarceloUrsulino <marceloursulino40@gmail.com>
2026-09-10 22:15:03 -0300

    feat: criando controller

---
+++ backend/controllers/ClienteController.js
@@ -0,0 +1,53 @@
+const cliente = require('../models/Cliente')
+const axios = require('axios')
+
+module.exports = class ClienteController{
+    static async ClienteCreate(req,res){
+        const { nome, email, cep, numero, complemento } = req.body
+
+
+        if(!nome || !email || !cep || !numero){
+            res.status(422).json({message: 'O campo precisa ser preenchido.'})
+            return
+        }
+
+        const somenteNumeros = cep.replace(/\D/g, '')
+
+        if(!cep || somenteNumeros.length !== 8){
+            res.status(400).json({message: 'Por favor, utilize um cep válido.'})
+            return
+           }
+        
+        try {
+
+            const resposta = await axios.get(`https://viacep.com.br/ws/${somenteNumeros}/json/`)
+
+            const dadosCep = resposta.data
+        
+            const rua = dadosCep.logradouro
+            const bairro = dadosCep.bairro
+            const cidade = dadosCep.localidade
+            const estado = dadosCep.uf
+
+            const create = await cliente.create({
+            nome,
+            email,
+            cep,
+            numero,
+            complemento,
+            rua,
+            bairro,
+            cidade,
+            estado
+        })
+        res.status(201).json({message: 'Cadastro criado.'})
+        return
+
+        } catch (error) {
+
+            res.status(400).json({message: 'Cep inválido.'})
+            
+        }
+    }
+
+}
